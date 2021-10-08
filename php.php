2fc851563466cca62c9351d8bcaa83dc5c44cd72
Muzammiljamil22 <81831067+Muzammiljamil22@users.noreply.github.com>
2021-10-08 20:31:01 +0500

    Add files via upload

---
+++ php.php
@@ -0,0 +1,52 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['login']) && isset($_POST['password']) &&
+    {
+        
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        $host = "localhost";
+        $dblogin = "root";
+        $dbPassword = "";
+        $dbName = "test";
+        $conn = new mysqli($host, $dblogin, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(login, password) values(?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+            if ($rnum == 0) {
+                $stmt->close();
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssssii",$login, $password);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
+}
+else {
+    echo "Submit button is not set";
+}
+?>
